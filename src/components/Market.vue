d861843c6ea3cce6c084d91269617347f69d4b0d
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-03-20 11:44:45 +0100

    data and collapsables on its way

--- src/components/Market.vue
+++ src/components/Market.vue
@@ -1,5 +1,22 @@
 <template>
-<h1>{{ info }}</h1>
+    <div class="market-values container pt-5">
+        <h3>Market Values</h3>
+        <p>{{ allData }}</p>
+        <div class="accordion" id="marketValue">
+            <div v-for="(data, index) in allDataAssets" class="card" :key="index">
+                <div class="card-header" id="MarketHeading">
+                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="'#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        {{ data.name }}
+                    </button>
+                </div>
+                    <div id="collapseOne" class="collapse show" aria-labelledby="MarketHeading" data-parent="#marketValue">
+                    <div class="card-body">
+                        {{ data.desc }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
   import axios from 'axios';
@@ -7,7 +24,9 @@
     name: 'Market',
       data() {
       return {
-        info: [],
+        allData: [],
+        allDataPrices: [],
+        allDataAssets: [],
       }
     },
     // async created(){
@@ -22,9 +41,10 @@
     // },
     created(){
       axios.get(`https://fake-stock-eye.herokuapp.com/`)
-        .then(res => {
-          console.log(res.data);
-          this.info = res.data;
+        .then(all => {
+            this.allData = all.data;
+            this.allDataPrices = all.data.prices;
+            this.allDataAssets = all.data.assets;
         })
         .catch(err => console.log(err));
     }
