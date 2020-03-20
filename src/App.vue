e454577e4472ea7ef64d1c4465cc363ee10430af
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-03-20 12:09:34 +0100

    migrated to bootstrapvue

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,22 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <router-link to="/" class="navbar-brand v-market-logo"><img alt="Visual Market logo" height="40" src="../src/assets/img/logo-land.png"></router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <router-link to="/about" class="nav-link">Market Values</router-link>
-          </li>
-          <li class="nav-item active">
-            <router-link to="/about" class="nav-link">Top Values</router-link>
-          </li>
-          <li class="nav-item active d-flex align-items-center">
-            <router-link to="/about" class="nav-link p-0"><i class="material-icons pt-1">bookmark</i></router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <b-navbar toggleable="lg">
+      <b-navbar-brand href="/"><img alt="Visual Market logo" height="40" src="../src/assets/img/logo-land.png"></b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/about">Market Values</b-nav-item>
+          <b-nav-item href="/about">Top Values</b-nav-item>
+          <b-nav-item href="/about"><i class="material-icons pt-1">bookmark</i></b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
     <footer class="d-flex justify-content-center">
       <img alt="Visual Market logo" height="100" src="../src/assets/img/logo-port.png">
     </footer>
   </div>
 </template>
-
-<style>
-  @import'~bootstrap/dist/css/bootstrap.css';
-</style>
 
 
 <script>
--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap'
+// import 'bootstrap'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import '../src/assets/styles/importer.css'
 
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
