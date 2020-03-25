98c961102e8b9fd33f4f338f5028aadb7ff6dce6
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-03-25 22:04:44 +0100

    some glitter

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -3,14 +3,12 @@
         <h3 class="mb-3 table-title">Top 3</h3>
         <div class="accordion" id="topMarketValue">
 			<div v-for="(data, index) in topData" class="card" :key="index">
-				<div class="card-header" :id="'heading-market' + data.id">
-					<button class="btn-market btn btn-link d-flex justify-content-between w-100" type="button" data-toggle="collapse" :data-target="'#collapse-market' + data.id" aria-expanded="false" :aria-controls="'collapse-market' + data.id">
-						<div class="left-side-els">
-							<img width="36" :src="require(`../../src/assets/img/markets/${data.id}.png`)">
-							<span>{{ data.symbol }}</span>
-							<span>{{ data.name }}</span>
-						</div>
-					</button>
+				<div class="card-header d-flex justify-content-between align-items-center">
+					<a :href="'#collapse-market' + data.id"><button class="left-side-els" type="button" data-toggle="collapse" :data-target="'#collapse-market' + data.id" aria-expanded="false" :aria-controls="'collapse-market' + data.id">
+						<img width="36" :src="require(`../../src/assets/img/markets/${data.id}.png`)">
+						<span>{{ data.symbol }}</span>
+						<span>{{ data.name }}</span>
+					</button></a>
 				</div>
 			</div>
         </div>
